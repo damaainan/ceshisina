9afbf512e3c4458d2e010f828fdc12e26a1d76bc
damaainan <damaainan@163.com>
2016-08-23 18:12:38 +0800

    x

--- Snoopy/cookietest.php
+++ Snoopy/cookietest.php
@@ -6,15 +6,47 @@ include("phpQuery/phpQuery.php");
 /**
  * 已生效
  */
-$snoopy = new Snoopy();
-$snoopy->agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.0.5) Gecko/2008120122 Firefox/3.0.5 FirePHP/0.2.1";//这项是浏览器信息，前面你用什么浏览器查看cookie，就用那个浏览器的信息(ps:$_SERVER可以查看到浏览器的信息)
-$snoopy->referer = "http://weibo.com/1730813174/profile?rightmod=1&wvr=6&mod=personnumber&is_all=1kkk";
-$snoopy->expandlinks = true;
-$snoopy->rawheaders["COOKIE"]="_s_tentry=login.sina.com.cn;ALF=1503470865;Apache=8794896837789.565.1471934869325;Hm_lvt_cdc2220e7553b2a2cd949e1765e21edc=1466418850,1466472305;SCF=AjbVfK4Xdw2XgTYyQGOIRtCsFHf_smxmyXZval-aDwjopr5G9Kf1oBHIgHzo2CLitKOAyWk89sLVq2bhxG6qfuM.;SINAGLOBAL=8062468627467.752.1458205942141;SSOLoginState=1471934866;SUB=_2A256v4HCDeTxGedJ6FIZ8S3NzDiIHXVZzPQKrDV8PUNbmtBeLWP5kW8BO1NKNFj4yJ7rmJPyZG4XFjRvPQ..;SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhOaEMKSk.Lxxebbn5-7OSC5JpX5KzhUgL.Fo2Ne05ReKepS0B2dJLoIpqLxKBLB.BLBoeLxKBLB.BLBoSNI0W9;SUHB=0jBZYyMZ2L3eDx;ULV=1471934869335:139:20:4:8794896837789.565.1471934869325:1471924462910;un=jiachunhui1988@sina.cn;UOR=news.ifeng.com,widget.weibo.com,login.sina.com.cn;wvr=6;YF-Ugrow-G0=57484c7c1ded49566c905773d5d00f82;YF-V5-G0=b188043973f8ae1849ba6cd9ae007290;";
+function init(){
+	$snoopy = new Snoopy();
+	$snoopy->agent = "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.0.5) Gecko/2008120122 Firefox/3.0.5 FirePHP/0.2.1";//这项是浏览器信息，前面你用什么浏览器查看cookie，就用那个浏览器的信息(ps:$_SERVER可以查看到浏览器的信息)
+	$snoopy->referer = "http://weibo.com/1730813174/profile?rightmod=1&wvr=6&mod=personnumber&is_all=1kkk";
+	$snoopy->expandlinks = true;
+	$snoopy->rawheaders["COOKIE"]="_s_tentry=login.sina.com.cn;ALF=1503470865;Apache=8794896837789.565.1471934869325;Hm_lvt_cdc2220e7553b2a2cd949e1765e21edc=1466418850,1466472305;SCF=AjbVfK4Xdw2XgTYyQGOIRtCsFHf_smxmyXZval-aDwjopr5G9Kf1oBHIgHzo2CLitKOAyWk89sLVq2bhxG6qfuM.;SINAGLOBAL=8062468627467.752.1458205942141;SSOLoginState=1471934866;SUB=_2A256v4HCDeTxGedJ6FIZ8S3NzDiIHXVZzPQKrDV8PUNbmtBeLWP5kW8BO1NKNFj4yJ7rmJPyZG4XFjRvPQ..;SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhOaEMKSk.Lxxebbn5-7OSC5JpX5KzhUgL.Fo2Ne05ReKepS0B2dJLoIpqLxKBLB.BLBoeLxKBLB.BLBoSNI0W9;SUHB=0jBZYyMZ2L3eDx;ULV=1471934869335:139:20:4:8794896837789.565.1471934869325:1471924462910;un=jiachunhui1988@sina.cn;UOR=news.ifeng.com,widget.weibo.com,login.sina.com.cn;wvr=6;YF-Ugrow-G0=57484c7c1ded49566c905773d5d00f82;YF-V5-G0=b188043973f8ae1849ba6cd9ae007290;";
+	return $snoopy;
+}
 
 
-// $snoopy->fetch("http://weibo.com/1730813174/profile?profile_ftype=1&is_all=1#_0");
+$page=3;
+$href="http://weibo.com/1730813174/profile?profile_ftype=1&is_all=1";
+$domain="100505";
+$id="1005051730813174";
+$href2="http://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=".$domain."&is_all=1&profile_ftype=1&pagebar=0&id=".$id;
+$total=[];
+for($i=1;$i<=$page;$i++){
+   if($i===1){
+   	$url=$href."#_0";
+   }else{
+   	$url=$href."&page=".$i;
+   }
+   $html=getHtml($url);
+   $arr=dealmain($html);
+   $total=array_merge($total,$arr);
+   for($j=0;$j<2;$j++){
+   	$url=$href2."&page=".$i."&pre_page=".$i."&pagebar=".$j;
+   	$html=getHtml($url);
+   	$arr=dealjs($html);
+   	$total=array_merge($total,$arr);
+   }
+}
+var_dump($total);
+/*
+$snoopy->fetch("http://weibo.com/1730813174/profile?is_search=0&visible=0&is_all=1&is_tag=0&profile_ftype=1&page=3#feedtop");
+              //http://weibo.com/1730813174/profile?is_search=0&visible=0&is_all=1&is_tag=0&profile_ftype=1&page=2#feedtop
+              //http://weibo.com/1730813174/profile?profile_ftype=1&is_all=1#_0
+              //http://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100505&is_all=1&profile_ftype=1&page=2&pagebar=0&pl_name=Pl_Official_MyProfileFeed__25&id=1005051730813174&script_uri=/1730813174/profile&feed_type=0&pre_page=2
+              //http://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100505&is_all=1&profile_ftype=1&page=2&pagebar=0&id=1005051730813174&pre_page=2&domain_op=100505
 $snoopy->fetch("http://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100505&profile_ftype=1&is_all=1&pagebar=1&pl_name=Pl_Official_MyProfileFeed__25&id=1005051730813174&script_uri=/1730813174/profile&feed_type=0&page=2&pre_page=2");
+*/
 // http://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100505&profile_ftype=1&is_all=1&pagebar=1&pl_name=Pl_Official_MyProfileFeed__25&id=1005051730813174&script_uri=/1730813174/profile&feed_type=0&page=1&pre_page=1
 // $snoopy->fetch("http://weibo.com/aj/mblog/fsearch?ajwvr=6&pre_page=1&page=1&pagebar=3");
 //              http://weibo.com/aj/mblog/fsearch?ajwvr=6&pre_page=1&page=1&end_id=4011665382921949&min_id=4011662849639487&pagebar=0&__rnd=1471939063454
@@ -27,14 +59,93 @@ $snoopy->fetch("http://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain=100505&p
 // $n=ereg_replace("href=\"","href=\"http://bbs.phpchina.com/",$snoopy->results );
 // $str=utf8_encode($snoopy->results);
 // http://weibo.com/aj/mblog/fsearch?ajwvr=6&pre_page=1&page=1&end_id=4011647544070677&min_id=4011645515081642&pagebar=0&__rnd=1471934983417
-$str=$snoopy->results;
-$str=str_replace('{"code":"100000","msg":"","data":"','',$str);
-$str=str_replace('/div>"}','',$str);
-$str=str_replace('\/','/',$str);
-$str=str_replace('\"','"',$str);
-$str=str_replace('\n','',$str);
-$str=str_replace('\r','',$str);
-$str=str_replace('\t','',$str);
+
+/*$str=$snoopy->results;
+$str=str_replace('{"code":"100000","msg":"","data":"','',$str);*/
+
+
+// $str=str_replace('/div>"}','',$str);
+// $str=str_replace('\/','/',$str);
+// $str=str_replace('\"','"',$str);
+// $str=str_replace('\n','',$str);
+// $str=str_replace('\r','',$str);
+// $str=str_replace('\t','',$str);
+
+
+/*$html=unicode_decode($str);*/
+
+function getHtml($url){
+	$snoopy=init();
+	$snoopy->fetch($url);
+	$str=$snoopy->results;
+	$str=str_replace('{"code":"100000","msg":"","data":"','',$str);
+	// $str=str_replace('/div>"}','',$str);
+	// $str=str_replace('\/','/',$str);
+	// $str=str_replace('\"','"',$str);
+	// $str=str_replace('\n','',$str);
+	// $str=str_replace('\r','',$str);
+	// $str=str_replace('\t','',$str);
+	$html=unicode_decode($str);
+	return $html;
+}
+
+function dealmain($html){
+
+// echo $html;
+// 处理直接拿到的数据
+	$pat='/<script>(.*?)<\/script>/i';
+	// $pat='/FM\.view\(\{(.*?)\)\}/i';
+	preg_match_all($pat, $html, $match);
+
+	// var_dump($match[1]);
+	foreach($match[1] as $ke=>$va){
+		// echo $va;
+		$str=str_replace('FM.view','',$va);
+		$str=ltrim($str,'(');
+		$str=rtrim($str,';');
+		$str=rtrim($str,')');
+        $darr=json_decode($str,true);
+        if(isset($darr['html'])){
+        	// echo $ke."***<br/>";
+        	 phpQuery::newDocument($darr['html']);
+        	 $resu=pq(".WB_detail>.WB_text");
+
+			$listarr=array();
+
+			foreach($resu as $v){
+				$larr=array();
+				$name=pq($v)->text();
+					$listarr[]=$name;//一维数组
+			}
+			// var_dump($listarr);
+			if(empty($listarr)){
+				continue;
+			}else{
+                break;
+			}
+        }
+        // var_dump($darr->html);
+	}
+	return $listarr;
+}
+
+
+function dealjs($html){
+
+
+// 处理js的数据
+phpQuery::newDocument($html);
+$resu=pq(".WB_detail>.WB_text");
+$listarr=array();
+
+		foreach($resu as $v){
+			$larr=array();
+			$name=pq($v)->text();
+				$listarr[]=$name;//一维数组
+		}
+	return $listarr;
+
+}
 
 function unicode_decode($name)
 {
@@ -61,24 +172,3 @@ function unicode_decode($name)
     }
     return $name;
 }
-$html=unicode_decode($str);
-// echo $html;
-phpQuery::newDocument($html);
-$resu=pq(".WB_detail>.WB_text");
-$listarr=array();
-
-		foreach($resu as $v){
-			$larr=array();
-			$name=pq($v)->text();
-				$listarr[]=$name;//一维数组
-		}
-		//*******************获取完整列表页**************************//
-	var_dump($listarr);
-
-
-// Semicolon separated Cookie File
-// This file was generated by EditThisCookie
-// Details: http://www.ietf.org/rfc/rfc2109.txt
-// Example: http://www.tutorialspoint.com/javascript/javascript_cookies.htm
-// URL: http://weibo.com/u/1730813174/home?wvr
-// _s_tentry=-;ALF=1498541822;Apache=6044112136587.501.1466991328525;Hm_lvt_cdc2220e7553b2a2cd949e1765e21edc=1466418850,1466472305;SINAGLOBAL=8062468627467.752.1458205942141;SSOLoginState=1467005823;SUB=_2A256dMsvDeTxGedJ6FIZ8S3NzDiIHXVZA7vnrDV8PUNbmtBeLWn6kW8S50K5Zxcv3k7sgsIH_w5KWx-jqQ..;SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WhOaEMKSk.Lxxebbn5-7OSC5JpX5K2hUgL.Fo2Ne05ReKepS0B2dJLoIpqLxKBLB.BLBoeLxKBLB.BLBoSNI0W9;SUHB=0wnkezqMM55pVa;ULV=1466991328530:89:30:1:6044112136587.501.1466991328525:1466761906060;un=jiachunhui1988@sina.cn;UOR=news.ifeng.com,widget.weibo.com,spr_web_360_hao360_weibo_t001;wvr=6;wb_bub_hot_1730813174=1;wb_g_minivideo_1730813174=1;WBStore=8ca40a3ef06ad7b2|undefined;YF-Page-G0=e3ff5d70990110a1418af5c145dfe402;YF-Ugrow-G0=ad06784f6deda07eea88e095402e4243;YF-V5-G0=b188043973f8ae1849ba6cd9ae007290;
