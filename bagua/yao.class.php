9ac46571982d8aa8f298213bd3ea5f72d2a21afd
damaainan <damaainan@163.com>
2016-12-27 17:17:00 +0800

    .

--- bagua/yao.class.php
+++ bagua/yao.class.php
@@ -34,25 +34,53 @@ class yao {
 	public function guaxiang($dayan){
 		$arr=$this->liuyao($dayan);//本卦
 		$bengua=$this->xiang($arr);
-		$narr=$this->bianyao($arr);//变卦
+		$resu=$this->bianyao($arr);//变卦
+		$narr=$resu[0];
+		$yaoci=$resu[1];
 		$biangua=$this->xiang($narr);
 		//检查得位  得中
-		$data=["ben"=>$arr,"bengua"=>$bengua,"bian"=>$narr,"biangua"=>$biangua];
+		$data=["ben"=>$arr,"bengua"=>$bengua,"bian"=>$narr,"biangua"=>$biangua,"yaoci"=>$yaoci];
 		return $data;
 		
 	}
+	function choose($arr){
+		$len=count($arr);
+		switch ($len){
+			case 0:;//用本卦的卦辞判断吉凶
+				break;
+			case 1:;//用本卦变爻的爻辞判断吉凶
+				break;
+			case 2:;//用本卦两个变爻的爻辞判断吉凶，但以居上的一爻的爻辞为主
+				break;
+			case 3:;//用本卦及之卦的卦辞判断吉凶，但以本卦卦辞为主，之卦卦辞为辅
+				break;
+			case 4:;//用之卦的两个不变爻的爻辞判断吉凶，但以居下一爻的爻辞为主
+				break;
+			case 5:;//用之卦的一个不变爻的爻辞判断吉凶
+				break;
+			case 6:;//用之卦的卦辞判断吉凶（若六爻皆为老阳，则用《乾》卦的“用九文
+			//辞判断吉凶；若六爻皆为老阴，则用《坤》卦的“用六”文辞判断吉凶。）
+				break;
+			
+		}
+	}
+	function checkWei(){}
+	function checkZhong(){}
 	function bianyao($arr){
 		$narr=[];
-		foreach ($arr as $v) {
+		$yaoci=[];
+		foreach ($arr as $k=>$v) {
 			if($v===6){
 				$narr[]=7;
+				$yaoci[]=$k;
 			}elseif($v===9){
 				$narr[]=8;
+				$yaoci[]=$k;
 			}else{
 				$narr[]=$v;
 			}
 		}
-		return $narr;
+		return [$narr,$yaoci];
 	}
 	function xiang($arr){
 		$gua=[];
