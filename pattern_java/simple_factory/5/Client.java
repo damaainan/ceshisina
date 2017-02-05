dc79262a3227952b026ba65372a33424d5cb279b
damaainan <damaainan@163.com>
2017-02-05 22:11:13 +0800

    .

---
+++ pattern_java/simple_factory/5/Client.java
@@ -0,0 +1,5 @@
+public class Client {
+    public static void main(String[] args) {
+        SingletonFactory.getSingleton().doSomething();
+    }
+}
---
+++ pattern_java/simple_factory/5/Singleton.java
@@ -0,0 +1,14 @@
+/**
+ * 单例对象
+ */
+public class Singleton {
+    
+    //不允许通过new产生一个对象
+    private Singleton(){
+        
+    }
+    
+    public void doSomething(){
+        //业务处理
+    }
+}
