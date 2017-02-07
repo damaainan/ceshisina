1590b3a4028aeb9f58787a326111c1c7af06a63a
damaainan <damaainan@163.com>
2017-02-07 15:05:12 +0800

    模板模式

---
+++ pattern_java/04template_method/2/Client.java
@@ -0,0 +1,13 @@
+/**
+ * 客户开始使用这个模型
+ */
+public class Client {
+
+	public static void main(String[] args) {
+		//牛叉公司要H1型号的悍马
+		HummerModel h1 = new HummerH1Model();
+		
+		//H1模型演示
+		h1.run();
+	}
+}
---
+++ pattern_java/04template_method/3/ConcreteClass2.java
@@ -0,0 +1,12 @@
+public class ConcreteClass2 extends AbstractClass {
+
+	//实现基本方法
+	protected void doAnything() {
+		//业务逻辑处理
+	}
+
+	protected void doSomething() {
+		//业务逻辑处理
+	}
+
+}
