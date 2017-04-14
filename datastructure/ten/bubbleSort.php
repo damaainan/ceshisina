7e60355386caa68117ffd7d07e4213b0bee58259
damaainan <damaainan@163.com>
2017-04-14 19:53:49 +0800

    .

--- datastructure/ten/bubbleSort.php
+++ datastructure/ten/bubbleSort.php
@@ -8,14 +8,13 @@ for ($i = 0; $i < 5000; $i++) {
 
 //2 冒泡排序
 
-function bubbleSort($arr)
-{
-
-    for ($i = 0; $i < count($arr); $i++) {
-        for ($j = 0; $j < $i + 1; $j++) {
-            if ($arr[$j] < $arr[$j - 1]) {
-                $temp = $arr[$j - 1];
-                $arr[$j - 1] = $arr[$j];
+function bubbleSort($arr){
+    $len=count($arr);
+    for ($i = 0; $i < $len; $i++) {
+        for ($j = 0; $j < $i - 1; $j++) {
+            if ($arr[$j +1 ] < $arr[$j]) {
+                $temp = $arr[$j + 1];
+                $arr[$j + 1] = $arr[$j];
                 $arr[$j] = $temp;
 
             }
