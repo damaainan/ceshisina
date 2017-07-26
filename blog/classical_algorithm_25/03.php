854472daa0d902e7ad58f6c10ffdb7114ec5eb97
damaainan <damaainan@163.com>
2017-07-26 18:31:54 +0800

    kong

--- blog/classical_algorithm_25/03.php
+++ blog/classical_algorithm_25/03.php
@@ -1,5 +1,46 @@
 <?php 
 header("Content-type:text/html; Charset=utf-8");
 /**
- * 
+ * 猴子吃桃
  */
+
+
+function Main()
+ {
+     $sum = SumPeach(1);
+
+     echo "第一天摘得桃子有:". $sum."\n";
+
+ }
+
+ //递归
+ function  SumPeach($day)
+ {
+     if ($day == 10)
+         return 1;
+
+     return 2 * SumPeach($day + 1) + 2;
+ }
+
+ Main();
+
+
+
+function Main2()
+{
+    $sum = SumPeachTail(1, 1);
+
+    printf("第一天摘得桃子有:%d", $sum);
+
+}
+
+//尾递归
+function SumPeachTail($day, $total)
+{
+    if ($day == 10)
+        return $total;
+
+    //将当前的值计算出传递给下一层
+    return SumPeachTail($day + 1, 2 * $total + 2);
+}
+Main2();
--- blog/classical_algorithm_25/05.php
+++ blog/classical_algorithm_25/05.php
@@ -1,5 +1,63 @@
 <?php 
 header("Content-type:text/html; Charset=utf-8");
 /**
- * 
+ * 字符串相似度
  */
+
+
+
+        function Main($str1,$str2)
+        {
+                
+                $martix = [strlen($str1) -1 ][strlen($str2) -1];
+
+                printf("字符串 %s 和 %s 的编辑距离为:%d\n", $str1, $str2, LD($str1,$str2));
+        }
+
+        /// <summary>
+        /// 计算字符串的编辑距离
+        /// </summary>
+        /// <returns></returns>
+        function LD($str1,$str2)
+        {
+            //初始化边界值(忽略计算时的边界情况)
+            for ($i = 0; $i < strlen($str1); $i++)
+            {
+                $martix[$i ][0] = $i;
+            }
+
+            for ($j = 0; $j < strlen($str2); $j++)
+            {
+                $martix[0][$j] = $j;
+            }
+
+            //矩阵的 X 坐标
+            for ($i = 0; $i < strlen($str1); $i++)
+            {
+                //矩阵的 Y 坐标
+                for ($j = 0; $j < strlen($str2); $j++)
+                {
+                    //相等情况
+                    if ($str1[$i - 1] == $str2[$j - 1])
+                    {
+                        $martix[$i][$j] = $martix[$i - 1][$j - 1];
+                    }
+                    else
+                    {
+                        //取“左前方”，“上方”，“左方“的最小值
+                        $temp1 = $martix[$i - 1][$j] < $martix[$i][$j - 1] ? $martix[$i - 1][$j] : $martix[$i][$j - 1] ;
+
+                        //获取最小值
+                        $min = $temp1 < $martix[$i - 1][$j - 1] ? $temp1 : $martix[$i - 1][$j - 1] ;
+
+                        $martix[$i][$j] = $min + 1;
+                    }
+                }
+            }
+
+            //返回字符串的编辑距离
+            return $martix[strlen($str1)][strlen($str2)];
+        }
+
+Main("abcd","egxg");
+Main("abde","dbae");
