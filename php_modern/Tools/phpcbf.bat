c3217884d093e14b1a2d126b7f9a91b1fdd383bd
damaainan <damaainan@163.com>
2017-12-20 17:23:35 +0800

    过不去
    过不去，郁结心中，有悲难去

---
+++ php_modern/Tools/phpcbf.bat
@@ -0,0 +1,22 @@
+::@echo off
+::REM PHP Code Beautifier and Fixer fixes violations of a defined coding standard.
+::REM 
+::REM @author    Greg Sherwood <gsherwood@squiz.net>
+::REM @copyright 2006-2015 Squiz Pty Ltd (ABN 77 084 670 600)
+::REM @license   https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt BSD Licence
+::
+::if "%PHP_PEAR_PHP_BIN%" neq "" (
+::    set PHPBIN=%PHP_PEAR_PHP_BIN%
+::) else set PHPBIN=php
+::
+::"%PHPBIN%" "%~dp0\phpcbf" %*
+
+
+@echo off
+if "%PHPBIN%" == "" set PHPBIN=D:\wamp64\bin\php\php5.6.31\php.exe
+if not exist "%PHPBIN%" if "%PHP_PEAR_PHP_BIN%" neq "" goto USE_PEAR_PATH
+GOTO RUN
+:USE_PEAR_PATH
+set PHPBIN=%PHP_PEAR_PHP_BIN%
+:RUN
+"%PHPBIN%" "D:\bin\PHP_CodeSniffer\bin\phpcbf" %*
