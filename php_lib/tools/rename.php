b2e1f7dcc846c8ce0c9e63f9e269647f3c5bf9aa
damaainan <damaainan@163.com>
2018-06-06 17:11:52 +0800

    tools

--- php_lib/tools/rename.php
+++ php_lib/tools/rename.php
@@ -1,4 +1,5 @@
 <?php 
+declare(strict_types=1);
 /**
  * Created by Sublime.
  * Date: 2018-06-04 18:34:47
@@ -7,27 +8,46 @@
 /**
  * @param  string $dir 名录名
  * @param  array  $ext 后缀范围
- * @param  string  $flag 切断标识
+ * @param  array  $flag 切断标识
  * @return void
  */
-function renameFiles(string $dir, array $ext, string $flag){
+function renameFiles(string $dir, array $ext, array $flag): void{
     if(!is_dir($dir)){
-        return false;
+        echo "非文件夹";
+        die;
     }
-    $dirArr = glob();
+    $dirArr = scandir($dir);
     // 扫描文件夹 
     foreach ($dirArr as $val) {
         if(is_dir($val)){
             // 设置切断标识 
-            $dirs = explode($flag, $val);
+            // $dirs = explode($flag, $val);// 或者字符串替换 循环替换 
+            $dirname = $val;
+            foreach ($flag as $fl) {
+                $val = str_replace($fl, '', $val); // 有数字 用正则 
+                // $val = preg_replace($fl, '', $val); // 有数字 用正则 
+            }
             // 处理 获取 名称
-            $dirname = $dirs[1];
-            $files = glob($dir . '/' . $val);
+            // $dirname = $dirs[1];
+            $nameflag = $val; // 要在文件名前加的前缀
+            $files = scandir($dir . $dirname);
             foreach ($files as $va) {
                 $arr = explode('.', $va);
-                if(in_array($arr[count[$arr] - 1], $ext){
+                $len = count($arr);
+                if(in_array($arr[$len - 1], $ext)){
+                    $oldname = $va;
+                    if(strpos($oldname, $nameflag) !== false){
+                        continue; // 已改名的跳过
+                    }
                     // 在给定的后缀范围内 重命名
-                    // 将获取的文件名加载名称之前 
+                    // 文件名 循环替换标识
+                    foreach ($flag as $fl) {
+                        $va = str_replace($fl, '', $va); // 有数字 用正则 
+                    }
+                    // 然后将获取的文件名加载名称之前 
+                    $newname = $nameflag . $va;
+                    // 重命名文件 
+                    rename($dir  . $dirname . '/' . $oldname, $dir . $dirname . '/' . $newname);
                 }
             }
         }
@@ -38,3 +58,6 @@ function renameFiles(string $dir, array $ext, string $flag){
     // 获得文件夹下需要重命名的文件类型 文件名
     // 
 }
+
+renameFiles('./',['txt'],['111']);
+// renameFiles('./',['txt'],['/[abcABC]-\d{1,2}/']);
--- php_lib/tools/sublime_keymap_to_phpstorm.php
+++ php_lib/tools/sublime_keymap_to_phpstorm.php
@@ -4,3 +4,6 @@
  * Date: 2018-05-31 10:34:40
  * sublime 快捷键映射生成 phpstorm 配置文件
  */
+// 思路 
+// 1. 查找 phpstorm 配置文件中的功能名 ，匹配 sublime 配置中的命令 替换 快捷键
+// https://github.com/dgallinari/phpstorm-sublime/blob/master/Sublime.xml  能用
